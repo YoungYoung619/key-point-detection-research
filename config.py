4682064ac401a8597328f870c264f44facc72008
YoungYoung619 <940752885@qq.com>
2019-10-09 17:02:03 +0800

    'config'

---
+++ config.py
@@ -0,0 +1,19 @@
+"""
+Copyright (c) College of Mechatronics and Control Engineering, Shenzhen University.
+All rights reserved.
+
+Description :
+
+
+Author：Team Li
+"""
+
+img_size = (511, 511)
+hm_size = (128, 128)
+
+min_radius_for_feedback_cal = 2
+radius_scalar = 10
+
+focal_loss_alpha = 2
+focal_loss_belta = 4
+
