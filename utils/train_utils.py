425aa02d4d9badffd29d3d98c0c5b0ea84b387e0
YoungYoung619 <940752885@qq.com>
2019-10-12 15:59:00 +0800

    'train_utils'

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -14,7 +14,48 @@ import cv2
 
 from utils.kp2hm_utils import heat_map_tf
 
-def get_feedback_hm_loss_for_each_bbox(hm_pred, bbox, class_index, radius_radio=3):
+def encode_for_one_bbox(hm_pred, emb_pred, offset_pred, bbox, class_index):
+    """encode
+    Args:
+        hm_pred: heat map prediction, with a shape [1, 128, 128, n_class]
+        emb_pred: embedding prediction, with a shape [1, 128, 128, 1]
+        offset_pred: offset prediction, with a shape [1 ,128, 128, 2]
+        bbox: ground truth bbox, [ymin, xmin, ymax, xmax], with a shape (4,)
+        class_index: class index, with a shape (1,)
+    Return:
+        heat map ground truth, with a shape [[1, 128, 128, n_class]]
+        a list of embedding pair, [key_point_1_embeding, key_point_2_embeding]
+
+    """
+
+
+def get_feedback_hm_loss_for_one_img(hm_pred, bboxes, classes):
+    """get feedback loss for one img
+    Args:
+        hm_pred: tensor, the net predition for the img in heat map, shape is (1, 128, 128, n_class)
+        bboxes: tensor, the groundtruth bbxoes for the img, val in [0, 1], [ymin, xmin, ymax, xmax], shape is (n_bboxes, 4)
+        classes: tensor, class index for the img, shape is (n_bboxes,)
+    Return:
+        group loss for each bbox, shape is (n_bboxes, 6)
+    """
+    def tfw_condition(i, group_loss):
+        return tf.less(i, tf.shape(bboxes)[0])
+
+    def tfw_body(i, group_loss):
+        group_loss_ = get_feedback_hm_loss_for_one_bbox(hm_pred, bboxes[i], classes[i])
+        group_loss_ = tf.concat([group_loss, tf.expand_dims(group_loss_, dim=0)], axis=0)
+        return i+1, group_loss_
+
+    i = tf.constant(1)
+    group_loss = get_feedback_hm_loss_for_one_bbox(hm_pred, bboxes[0], classes[0])
+    group_loss = tf.expand_dims(group_loss, dim=0)
+    i, group_loss = tf.while_loop(tfw_condition, tfw_body, [i, group_loss],
+                                  shape_invariants=[i.get_shape(), tf.TensorShape([None, group_loss.get_shape()[1]])])
+
+    return group_loss
+
+
+def get_feedback_hm_loss_for_one_bbox(hm_pred, bbox, class_index, radius_radio=3):
     """get feed back loss for each bbox
     Args:
         hm_pred: net predition for heat map, with a shape [1, 128, 128, n_class], value is in [0., 1.]
@@ -37,14 +78,14 @@ def get_feedback_hm_loss_for_each_bbox(hm_pred, bbox, class_index, radius_radio=
         y_index = tf.cast(point[1] * config.hm_size[0], tf.int32)
         x_index = tf.cast(point[0] * config.hm_size[1], tf.int32)
 
-        pos_pred = tf.gather_nd(hm_pred, [0, y_index, x_index, class_index])
+        pos_pred = tf.gather_nd(hm_pred, [0, y_index, x_index, tf.cast(class_index, tf.int32)])
         consider_radius = tf.maximum(bbox_min_len, config.min_radius_for_feedback_cal)
 
         y_begin = tf.maximum(y_index-consider_radius, 0)
         x_begin = tf.maximum(x_index-consider_radius, 0)
         size_y = tf.minimum(consider_radius + y_index - y_begin, config.hm_size[0] - y_begin)
         size_x = tf.minimum(consider_radius + x_index - x_begin, config.hm_size[1] - x_begin)
-        consider_range_pred = tf.squeeze(tf.slice(hm_pred, begin=(0, y_begin, x_begin, class_index), size=(1, size_y, size_x, 1)))
+        consider_range_pred = tf.squeeze(tf.slice(hm_pred, begin=(0, y_begin, x_begin, tf.cast(class_index, tf.int32)), size=(1, size_y, size_x, 1)))
 
         pos_loss = -tf.pow(1 - pos_pred, config.focal_loss_alpha)*tf.log(pos_pred)
 
@@ -88,22 +129,28 @@ def get_feedback_hm_loss_for_each_bbox(hm_pred, bbox, class_index, radius_radio=
 
 
 if __name__ == '__main__':
+    ## test-1 for get_feedback_hm_loss_for_each_bbox
+    # net_pred = tf.placeholder(shape=(None, 128, 128, 80), dtype=tf.float32)
+    # bbox = tf.placeholder(shape=(4), dtype=tf.float32)
+    # pl = get_feedback_hm_loss_for_each_bbox(net_pred, bbox=bbox, class_index=23)
+    #
+    # with tf.Session() as sess:
+    #     for i in range(100):
+    #         c = np.random.uniform(0, 1, (10, 128, 128, 80))
+    #
+    #         b = np.random.uniform(0, 1, 2)
+    #         xmin = np.min(b)
+    #         xmax = np.max(b)
+    #         cc = np.random.uniform(0, 1, 2)
+    #         ymin = np.min(cc)
+    #         ymax = np.max(cc)
+    #
+    #         apll = sess.run(pl, feed_dict={net_pred: c, bbox:np.array([ymin, xmin, ymax, xmax])})
+    #         pass
+
+    ## test-2 for get_feedback_hm_loss_for_one_img
     net_pred = tf.placeholder(shape=(None, 128, 128, 80), dtype=tf.float32)
-    bbox = tf.placeholder(shape=(4), dtype=tf.float32)
-    pl = get_feedback_hm_loss_for_each_bbox(net_pred, bbox=bbox, class_index=23)
-
-    with tf.Session() as sess:
-        for i in range(100):
-            c = np.random.uniform(0, 1, (10, 128, 128, 80))
-
-            b = np.random.uniform(0, 1, 2)
-            xmin = np.min(b)
-            xmax = np.max(b)
-            cc = np.random.uniform(0, 1, 2)
-            ymin = np.min(cc)
-            ymax = np.max(cc)
-
-            apll = sess.run(pl, feed_dict={net_pred: c, bbox:np.array([ymin, xmin, ymax, xmax])})
-            pass
-
+    bboxes = tf.placeholder(shape=(None, 4), dtype=tf.float32)
+    labels = tf.placeholder(shape=(None), dtype=tf.int64)
+    get_feedback_hm_loss_for_one_img(net_pred, bboxes, labels)
     pass
